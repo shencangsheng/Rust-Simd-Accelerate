93093ecb53ac6b6b9b26da873b1612c52ca40429
shencangsheng <shencangsheng@126.com>
2021-11-21 22:16:10 +0800

    feat: Simd On

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,67 @@
 #![feature(portable_simd)]
-use std::{ptr::null, simd::u8x16};
+#![allow(non_camel_case_types)]
+#[macro_use]
+extern crate arrayref;
+use std::{collections::HashMap, fmt::Result, result};
 
-use core_simd::*;
+use core_simd::{u8x16, u8x32, u8x4, u8x64, u8x8, Simd};
+
 fn main() {
     let s1 = u8x4::from_array([1, 1, 0, 0]);
     let s2 = u8x4::from_array([1, 0, 0, 1]);
     println!("{:?}", s1 & s2);
-    exec([64, 32, 16, 8, 4], 0, &[1], &[2])
+    let result: &[u8] = &[];
+    exec(&[32, 16, 8, 4], 0, &[0; 64], &[0; 64], result);
+    println!("{:#?}", &result);
 }
 
-fn exec<T>(register: T, i: i32, a: &[u8], b: &[u8]) {
+fn exec(register: &[i32], mut i: i32, a: &[u8], b: &[u8], result: &[u8]) {
     let size = a.len() as i32;
     let last = size - i;
-    let count: Option<i32>;
-    let next: Option<i32>;
     for x in register.iter() {
         if last > *x {
-            count = Some(last / x);
-            next = Some(last % x);
-            break;
+            let count = last / x;
+            let next = last % x;
+            for element in 0..count {
+                match x {
+                    32 => {
+                        addArray(
+                            result,
+                            &(u8x32::from_array(*array_ref![&a, i as usize, 32])
+                                & u8x32::from_array(*array_ref![&b, i as usize, 32]))
+                            .to_array(),
+                        );
+                    }
+                    16 => {
+                        addArray(
+                            result,
+                            &(u8x16::from_array(*array_ref![&a, i as usize, 16])
+                                & u8x16::from_array(*array_ref![&b, i as usize, 16]))
+                            .to_array(),
+                        );
+                    }
+                    8 => {
+                        addArray(
+                            result,
+                            &(u8x8::from_array(*array_ref![&a, i as usize, 8])
+                                & u8x8::from_array(*array_ref![&b, i as usize, 8]))
+                            .to_array(),
+                        );
+                    }
+                    4 => {
+                        addArray(
+                            result,
+                            &(u8x4::from_array(*array_ref![&a, i as usize, 4])
+                                & u8x4::from_array(*array_ref![&b, i as usize, 4]))
+                            .to_array(),
+                        );
+                    }
+                    _ => unreachable!(),
+                }
+                i += x - 1;
+            }
         }
     }
-
-    if count.is_some() && next.is_some() {}
 }
+
+fn addArray(mut s1: &[u8], s2: &[u8]) {}
